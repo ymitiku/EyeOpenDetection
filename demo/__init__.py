36baa48ad8f1a3d811503a61f214cca74bceecc5
Mitiku <se.mitiku.yohannes@gmail.com>
2018-02-15 13:00:22 +0300

    added demo for trained model

--- demo/__init__.py
+++ demo/__init__.py
@@ -3,7 +3,9 @@ import numpy as np
 import os
 import cv2
 import dlib
-
+from keras.models import model_from_json
+import json
+from nets.model import EyeClosedModel
 def split_train_test_left_eye_dataset(dataset_dir):
     file_locations = []
     opened = []
@@ -56,3 +58,75 @@ def split_train_test_right_eye_dataset(dataset_dir):
     train.to_pickle(os.path.join(dataset_dir,"train_right.pkl"))
     test.to_pickle(os.path.join(dataset_dir,"test_right.pkl"))
     print "splitted right eye dataset"
+
+def get_dlib_points(img,predictor,rectangle):
+    shape = predictor(img,rectangle)
+    dlib_points = np.zeros((68,2))
+    for i,part in enumerate(shape.parts()):
+        dlib_points[i] = [part.x,part.y]
+    return dlib_points
+def get_left_eye(face_img,dlib_points):
+    left_eye_left = int(max(dlib_points[36][0]-5,0))
+    left_eye_top = int(max(min(dlib_points[37][1],dlib_points[38][1])-5,0))
+    left_eye_right = int(min(face_img.shape[1],dlib_points[39][0]+5))
+    left_eye_bottom = int(min(max(dlib_points[40][1],dlib_points[41][1])+5,face_img.shape[0]))
+    left_eye = face_img[left_eye_top:left_eye_bottom,left_eye_left:left_eye_right]
+
+    return left_eye
+def get_right_eye(face_img,dlib_points):
+    right_eye_left = int(max(dlib_points[42][0]-5,0))
+    right_eye_top = int(max(min(dlib_points[43][1],dlib_points[45][1])-5,0))
+    right_eye_right = int(min(face_img.shape[1],dlib_points[45][0]+5))
+    right_eye_bottom = int(min(max(dlib_points[46][1],dlib_points[47][1])+5,face_img.shape[0]))
+    right_eye = face_img[right_eye_top:right_eye_bottom,right_eye_left:right_eye_right]
+
+    return right_eye
+
+def eye_open_webcam_demo(model_path):
+    eyeclosed_model = EyeClosedModel((24,24,1))
+    model = eyeclosed_model.model
+    model.load_weights(model_path)
+    cap = cv2.VideoCapture(-1)
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+    # model.summary()
+    while cap.isOpened():
+        _,frame = cap.read()
+        gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+        faces = detector(gray)
+        for i,face in enumerate(faces):
+            face_img = gray[
+                    max(0,face.top()):min(gray.shape[0],face.bottom()),
+                    max(0,face.left()):min(gray.shape[1],face.right())
+            ]
+            cv2.rectangle(frame,(face.left(),face.top()),(face.right(),face.bottom()),color=(255,0,0),thickness=2)
+            face_img = cv2.resize(face_img,(100,100))
+            dlib_points = get_dlib_points(face_img,predictor,dlib.rectangle(0,0,face_img.shape[1],face_img.shape[0]))
+            left_eye  = get_left_eye(face_img,dlib_points)
+            left_eye = cv2.resize(left_eye,(24,24))
+            right_eye = get_right_eye(face_img,dlib_points)
+            right_eye = cv2.resize(right_eye,(24,24))
+            left_eye = left_eye.astype(np.float32)/255
+            right_eye = right_eye.astype(np.float32)/255
+            left_prediction = model.predict(left_eye.reshape(-1,24,24,1))[1]
+            right_prediction = model.predict(right_eye.reshape(-1,24,24,1))[0]
+            left_arg_max = np.argmax(left_prediction)
+            right_arg_max = np.argmax(right_prediction)
+            if left_arg_max ==0:
+                left_text = "Left eye Closed"
+            else:
+                left_text = "Left eye Opened"
+            if right_arg_max ==0:
+                right_text = "Right eye Closed"
+            else:
+                right_text = "Right eye Opened"
+
+            cv2.putText(frame,left_text,(face.left()+10,face.top()+10), cv2.FONT_HERSHEY_DUPLEX, 0.4,color=(0,0,255),thickness=1)
+            cv2.putText(frame,right_text,(face.left()+10,face.top()+30), cv2.FONT_HERSHEY_DUPLEX, 0.4,color=(0,0,255),thickness=1)
+            cv2.imshow("Frame",frame)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+    cv2.destroyAllWindows()
+    cap.release()
+            
+        
--- demo/__main__.py
+++ demo/__main__.py
@@ -1,68 +1,28 @@
 import cv2
 import dlib
 import numpy as np
-from demo import split_train_test_left_eye_dataset,split_train_test_right_eye_dataset
+from demo import eye_open_webcam_demo,split_train_test_left_eye_dataset,split_train_test_right_eye_dataset
 import argparse
 def get_cmd_args():
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--dataset_dir",default="",type=str)
     parser.add_argument("--left",type=bool)
+    parser.add_argument("--process",default="webcam_demo",type=str)
 
     args = parser.parse_args()
     return args
 
 def main():
-    # img = cv2.imread("imgs/image00041.jpg")
-    # height= img.shape[0]
-    # width = img.shape[1]
-    # h = 720
-    # scale = h/float(height)
-    # w = scale * width 
-    # img = cv2.resize(img,(int(w),int(h)))
-
-    # detector = dlib.get_frontal_face_detector()
-    # faces = detector(img)
-    # predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-    # face = faces[0]
-    # cv2.rectangle(img,(face.left(),face.top()),(face.right(),face.bottom()),color=(255,0,0),thickness=2)
-    # dlib_points = np.zeros((68,2))
-    # f_top = face.top()
-    # f_left = face.left()
-    # f_right = face.right()
-    # f_bottom = face.bottom()
-
-    # face_img = img[f_top:f_bottom,f_left:f_right]
-    # face_img = cv2.resize(face_img,(100,100))
-    # shape = predictor(face_img,dlib.rectangle(0,0,face_img.shape[1],face_img.shape[0]))
-
-
-
-    # for i,part in enumerate(shape.parts()):
-    #     # if i in range(36,42):
-    #         # cv2.circle(face_img,(part.x,part.y),2,color=(0,255,0),thickness=2)
-    #     if i in range(42,48):
-    #         cv2.circle(face_img,(part.x,part.y),2,color=(0,0,255),thickness=2)
-    #     dlib_points[i] = [part.x,part.y]
-    
-    # left_eye_left = int(max(dlib_points[36][0]-5,0))
-    # left_eye_top = int(max(min(dlib_points[37][1],dlib_points[38][1])-5,0))
-    # left_eye_right = int(min(face_img.shape[1],dlib_points[39][0]+5))
-    # left_eye_bottom = int(min(max(dlib_points[40][1],dlib_points[41][1])+5,face_img.shape[0]))
-
-    # left_eye = face_img[left_eye_top:left_eye_bottom,left_eye_left:left_eye_right]
-    # left_eye = cv2.resize(left_eye,(24,24))
-    # cv2.imshow("left_eye",left_eye)
-    # cv2.imshow("face",face_img)
-    # cv2.imshow("Image",img)
-    # cv2.waitKey(0)
-    # cv2.destroyAllWindows()
+   
     args = get_cmd_args()
-    print args.left
-    if args.left:
-        split_train_test_left_eye_dataset(args.dataset_dir)
-    else:
-        split_train_test_right_eye_dataset(args.dataset_dir)
+    if args.process == "webcam_demo":
+        eye_open_webcam_demo("models/left2.h5")
+    elif args.process == "split dataset":
+        if args.left:
+            split_train_test_left_eye_dataset(args.dataset_dir)
+        else:
+            split_train_test_right_eye_dataset(args.dataset_dir)
 
 if __name__ == "__main__":
     main()
